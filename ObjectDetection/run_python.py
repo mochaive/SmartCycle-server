4fbe075fd078ef10cea7389337cc02ee503d2ee4
whoisStarBox <y2142144@gmail.com>
2020-07-03 20:53:57 +0900

    해당 디렉토리 삭제

--- ObjectDetection/run_python.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-import subprocess
-cwd = os.getcwd()
-
-proc = subprocess.Popen(['python3', cwd+"/classifier.py"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-output = proc.communicate()[0].decode("utf-8")
-print(output)
